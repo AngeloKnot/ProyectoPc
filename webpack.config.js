8c3f53d166d17267f09fa45d23f212ced3e8a173
adolfo <adolfo9angelo@gmail.com>
2022-09-26 08:50:28 -0500

    webpack configurado

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+//el archivo de configuracion debe usar ES5
+
+//importar un administrador de rutas de archivos 
+const path = require('path');
+
+//exportamos un objeto de configuraion
+//que sera usado por el webpack
+module.exports = {
+    //1. El archivo de entrada o indexador
+    entry: "./client/index.js",
+    //2. Especificar el archivo de salida
+    output: {
+        //2.1 Ruta absoluta de salida
+        path: path.resolve(__dirname, "public"),
+        //2.2 Nombre del archivo de salida
+        filename: "bundle.js"
+    },
+    //3. configurando el servidor de desarrollo
+    devServer: {
+        //3.1 Folder de archivos estaticos
+        static: path.join(__dirname, "public"),
+        //3.2 Puerto del servidor de desarrollo
+        port: 8080,
+        //3.3 Definiendo el host
+        host: "localhost"
+    }
+
+}
