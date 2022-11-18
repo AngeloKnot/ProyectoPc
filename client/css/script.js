83ecc868d42fd989dbabfdd8f8bfe974406bf90a
adolfo <adolfo9angelo@gmail.com>
2022-11-17 23:52:09 -0600

    primeras vistas al proyecto

---
+++ client/css/script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+let close_button = document.getElementById('close-button');
+close_button.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById("window-notice").style.display = "none";
+});
